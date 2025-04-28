6b0a30eb06254543d86b0b45f3e72877cef85ec7
Emberate <emberate147@gmail.com>
2025-04-28 12:07:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
+    server: {
+    port: 5173, // Set the server port to 5173
+  },
 
   ],
 })
